8f35516a6e04426655d482a4fe8f41afbd08cf83
JovisFilius <42693100+JovisFilius@users.noreply.github.com>
2022-10-02 18:45:07 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,37 @@
+# elm-linear-sRGB
+
+The standard RGB encoding
+([sRGB](https://en.wikipedia.org/wiki/SRGB#The_forward_transformation_.28CIE_xyY_or_CIE_XYZ_to_sRGB.29))
+is the colorspace currently in use by the vast majority of programs and
+websites. This encoding is not linear with respect to the perceived lightness
+of the resulting color by the human eye. One consequence of this is that
+(linear) interpolations between two sRGB-encoded color values tend to produce
+murky intermediary colors with varying perceived lightness, since the color
+values have already been non-linearly transformed to sRGB.
+
+In contrast, after decoding sRGB values back to the
+[CIE RGB](https://en.wikipedia.org/wiki/CIE_1931_color_space) 
+space, linearly interpolating between the red, green and blue components yields
+colors that all share the same perceived brightness.
+
+## Visual comparison
+
+![greenToBlue.png]
+
+In the above screenshot, the upper interpolation is the current 'standard'
+interpolation applied to the sRGB-encoded values. The lower interpolation is
+the interpolation applied after decoding the sRGB to (linear) CIE-space.
+
+## Documentation
+
+This package currently exports a single utility function that performs the
+respective decoding and re-encoding from and to sRGB internally and yields true
+linear interpolations between the given sRGB-encoded colors. The integer
+argument determines the number of colors to be produced between the given start
+and endpoint.
+
+```elm
+linearInterpolation : SRGB -> SRGB -> Int -> List SRGB
+
+type alias SRGB = (Float, Float, Float)
+```
--- src/ByJovis/Color.elm
+++ src/ByJovis/Color.elm
@@ -28,15 +28,18 @@ main = Browser.sandbox
 
 
 type alias Model =
-    { start : Color
-    , end : Color
+    { start : SRGB
+    , end : SRGB
     , nSteps : Int
     }
 
 
+type alias SRGB = (Float, Float, Float)
+
+
 init =
-    { start = rgb 0 1 0
-    , end = rgb 0 0 1
+    { start = (0.5,0,1)
+    , end = (0,1,0)
     , nSteps = 1000
     }
 
@@ -59,25 +62,25 @@ cRange start end nInter =
         List.map (\i -> start + (toFloat i) * step) <| List.range 0 (nInter+1)
 
 
-red : Color -> Float
-red = .red << toRgb
-green : Color -> Float
-green = .green << toRgb
-blue : Color -> Float
-blue = .blue << toRgb
+-- red : Color -> Float
+-- red = .red << toRgb
+-- green : Color -> Float
+-- green = .green << toRgb
+-- blue : Color -> Float
+-- blue = .blue << toRgb
 
 
-wrongInterpolation : Color -> Color -> Int -> List Color
-wrongInterpolation c1 c2 n =
+wrongInterpolation : SRGB -> SRGB -> Int -> List SRGB
+wrongInterpolation (r1,g1,b1) (r2,g2,b2) n =
     List.map3
-        rgb
-        (cRange (red c1) (red c2) n)
-        (cRange (green c1) (green c2) n)
-        (cRange (blue c1) (blue c2) n)
+        (\r g b -> (r,g,b))
+        (cRange (r1) (r2) n)
+        (cRange (g1) (g2) n)
+        (cRange (b1) (b2) n)
 
 
-linearInterpolation : Color -> Color -> Int -> List Color
-linearInterpolation c1 c2 n =
+linearInterpolation : SRGB -> SRGB -> Int -> List SRGB
+linearInterpolation (r1,g1,b1) (r2,g2,b2) n =
     let
         decode cs =
             if cs <= 0.04045 then
@@ -91,15 +94,15 @@ linearInterpolation c1 c2 n =
             else
                 1.055 * cl^(1/2.4) - 0.055
 
-        rl1 = decode <| red c1
-        rl2 = decode <| red c2
-        gl1 = decode <| green c1
-        gl2 = decode <| green c2
-        bl1 = decode <| blue c1
-        bl2 = decode <| blue c2
+        rl1 = decode r1 --<| red c1
+        rl2 = decode r2 --<| red c2
+        gl1 = decode g1 --<| green c1
+        gl2 = decode g2 --<| green c2
+        bl1 = decode b1 --<| blue c1
+        bl2 = decode b2 --<| blue c2
     in
         List.map3
-            (\r g b -> rgb (encode r) (encode g) (encode b))
+            (\r g b -> ((encode r),(encode g),(encode b)))
             (cRange rl1 rl2 n) (cRange gl1 gl2 n) (cRange bl1 bl2 n)
 
 
@@ -137,20 +140,20 @@ view model =
                 , height fill
                 ]
                 <| List.map
-                    (colorView << fromRgb << C.toRgba)
+                    (colorView << (\{red,green,blue} -> (red,green,blue)) << C.toRgba)
                     <| builtinInterpolation
-                        (C.fromRgba <| toRgb model.start)
-                        (C.fromRgba <| toRgb model.end)
+                        ((\(r,g,b) -> C.rgb r g b) model.start)
+                        ((\(r,g,b) -> C.rgb r g b) model.end)
                         model.nSteps
             ]
 
 
-colorView : Color -> Element msg
-colorView color =
+colorView : SRGB -> Element msg
+colorView (r,g,b) =
     el
         [ height fill
         , width fill
-        , Background.color color
+        , Background.color <| rgb r g b
         ]
         none
         -- <| text <|
